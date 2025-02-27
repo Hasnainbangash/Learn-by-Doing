34545ae8373409dfe95b307b80f7a09492718a4f
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-27 17:29:51 +0500

    Initial Commit

---
+++ Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Elexoft on 27/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Learn by DoingUITests/Learn_by_DoingUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Learn_by_DoingUITestsLaunchTests.swift
+//  Learn by DoingUITests
+//
+//  Created by Elexoft on 27/02/2025.
+//
+
+import XCTest
+
+final class Learn_by_DoingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
