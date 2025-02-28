9c9f48022208b75d898944af3098bd84fa1775a5
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-28 12:35:06 +0500

    Showing laert work done on each button pressed

--- Learn by Doing/CardView.swift
+++ Learn by Doing/CardView.swift
@@ -15,6 +15,7 @@ struct CardView: View {
     @State private var fadeIn: Bool = false
     @State private var moveDownward: Bool = false
     @State private var moveUpward: Bool = false
+    @State private var showAlert: Bool = false
     
     var hapticImpact = UIImpactFeedbackGenerator(style: .heavy)
     
@@ -43,10 +44,9 @@ struct CardView: View {
             
             Button(action: {
                 print("Button was tapped.")
-                
                 playSound(sound: "sound-chime", type: "mp3")
-                
                 hapticImpact.impactOccurred()
+                self.showAlert.toggle()
             }) {
                 HStack {
                     Text(card.callToAction.uppercased())
@@ -78,6 +78,13 @@ struct CardView: View {
                 self.moveDownward.toggle()
                 self.moveUpward.toggle()
             }
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text(card.title),
+                message: Text(card.message),
+                dismissButton: .default(Text("OK"))
+            )
         }
     }
 }
