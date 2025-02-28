b248a48dd9a433d7b10e066c0366285fadc7b7c1
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-28 11:48:13 +0500

    Card view is added in the content view with the card data

--- Learn by Doing/ContentView.swift
+++ Learn by Doing/ContentView.swift
@@ -10,14 +10,15 @@ import SwiftUI
 struct ContentView: View {
     // MARK: - PROPERTIES
     
+    var cards: [Card] = cardData
+    
     // MARK: - CONTENT
     
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(alignment: .center, spacing: 20) {
-                ForEach(0 ..< 6) { item in
-                    // CardView()
-                    Text("Card")
+                ForEach(cards) { item in
+                    CardView(card: item)
                 }
             } //: HSTACK
             .padding(20)
